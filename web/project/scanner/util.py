043452a2a980db98fe10381a1318fcd422e8e4bf
Matt Fuller <matthewdf10@gmail.com>
2012-07-23 10:54:39 -0700

    adding initial web
    adding playdoh project and frontend in web

---
+++ web/project/scanner/util.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.conf import settings
+from project import scanner
+import logging
+import commonware
+
+log = commonware.log.getLogger('playdoh')
+
+#Function called from settings page which prevents user account creation if email not from accepted domain
+def create_user(email):
+    domain = email.rsplit('@', 1)[1]
+    for i, v in enumerate(settings.ACCEPTED_USER_DOMAINS):
+        if domain == v:
+            return User.objects.create_user(email, email)
+            #log.debug("WOULD HAVE added user from " + domain)
